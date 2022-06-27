1efa97ea86aa97134e4f8feb94e4dc47545d71e4
zishan <macone394@gmail.com>
2022-06-27 14:21:54 +0530

    m

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BooklistingComponent } from './booklisting/booklisting.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { BooklistingComponent } from './booklisting/booklisting.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/booklisting/booklisting.component.ts
+++ src/app/booklisting/booklisting.component.ts
@@ -6,42 +6,20 @@ import { MOCK_DATA } from './data/data';
   styleUrls: ['./booklisting.component.scss']
 })
 export class BooklistingComponent implements OnInit {
-  fullAddress:string="";
+  fullAddress: string = "";
   bookList: Array<any> = MOCK_DATA;
 
   constructor() { }
 
   ngOnInit(): void {
     console.log(this.bookList);
-    // console.log(this.bookList[0]);
-
   }
 
-
-
-  getFullName(name:any){
-    return name.firstname+" "+name.lastname;
+  getFullName(name: any) {
+    return `${name.firstname} ${name.lastname}`;
   }
 
-getFullAddress(region:any){
-
-  // this.fullAddress="";
-  // for(let location of Object.values(region))
-  // {
-    // this.fullAddress+=location;
-
-  // }
-  return region.city+" "+","+" "+region.state+" "+","+" "+region.country+" "+","+" "+region.address+" "+","+" "+region.zipcode;
-
-}
-
-getEditorName(editor:any)
-{
-  return editor.firstname+" "+editor.lastname;
-}
-
-
-
-
-
+  getFullAddress(region: any) {
+    return `${region.address}, ${region.city}, ${region.state}, ${region.country}, ${region.zipcode}`;
+  }
 }
