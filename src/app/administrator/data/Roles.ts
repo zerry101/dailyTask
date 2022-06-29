7923382a06d07bec569f86bce88fc2e29b1a5526
Biren Yadav <byadav@masaglobal.com>
2022-06-29 12:07:28 +0530

    m

--- src/app/administrator/data/Roles.ts
+++ src/app/administrator/data/Roles.ts
@@ -2,9 +2,9 @@
 export
 
   const Roles: any = [
-    { category: "Admin"   , permissions: ["Create ", "Read", "Update", "Delete"], active:false },
-    { category: "SuperAdmin", permissions: ["Create ", "Read", "Update", "Delete", "Modify"], active: false },
-    { category: "User", permissions: ["Read"], active: false },
-    { category: "Manager", permissions: ["Read", "Update"], active: false }
+    { category: "Admin"   , permissions: {Create:true , Read:true, Update:true, Delete:true,Modify:false}, active:false, },
+    { category: "SuperAdmin", permissions: {Create:true, Read:true, Update:true, Delete:true, Modify:true}, active: false },
+    { category: "User", permissions: {Create:false,Read:true,Update:false,Delete:false,Modify:false}, active: false },
+    { category: "Manager", permissions: {Create:false,Read:true, Update:true,Delete:false,Modify:false}, active: false }
   ]
 ;
